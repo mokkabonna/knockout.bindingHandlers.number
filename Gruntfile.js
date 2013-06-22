f9d54e0acdb82f1ddb6bcc84b3655c6e874e2f89
Martin Hansen <martin@martinhansen.no>
2013-06-22 16:52:08 +0200

    Automated bower install

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,35 @@
-'use strict';
-module.exports = function(grunt) {
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		qunit: {
-			all: ['test/**/*.html']
-		},
-		watch: {
-			scripts: {
-				files: ['**/*.js', 'test/**'],
-				tasks: ['qunit']
+(function() {
+	'use strict';
+	module.exports = function(grunt) {
+		// Project configuration.
+		grunt.initConfig({
+			pkg: grunt.file.readJSON('package.json'),
+			jshint: {
+				all: ['Gruntfile.js', 'src/**/*.js']
 			},
-		}
-	});
+			qunit: {
+				all: ['test/**/*.html']
+			},
+			watch: {
+				scripts: {
+					files: ['**/*.js', 'test/**'],
+					tasks: ['qunit', 'jshint']
+				}
+			},
+			bower: {
+				install: {
 
-	grunt.loadNpmTasks('grunt-contrib-qunit');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+				}
+			}
+		});
 
-	// Default task(s).
-	grunt.registerTask('test', ['qunit', 'watch:scripts']);
-	grunt.registerTask('default', ['qunit']);
-};
+		grunt.loadNpmTasks('grunt-contrib-qunit');
+		grunt.loadNpmTasks('grunt-contrib-jshint');
+		grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-bower-task');
+
+		// Default task(s).
+		grunt.registerTask('test', ['bower:install', 'jshint', 'qunit', 'watch:scripts']);
+		grunt.registerTask('default', ['bower:install', 'qunit', 'jshint']);
+	};
+})();
