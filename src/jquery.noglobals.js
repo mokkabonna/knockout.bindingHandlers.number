8535b449836f75bbb60acaed96452523db39ece5
Martin Hansen <martin@martinhansen.no>
2013-06-26 22:57:02 +0200

    gh-pages first semi-automated commit

---
+++ src/jquery.noglobals.js
@@ -0,0 +1,5 @@
+define(['../lib/jquery/jquery.js'], function() {
+	var jquery = $; //store original jquery before deleting it from the global namespace
+	$.noConflict(true); //delete $ and jQuery from the global namespace
+	return jquery; //return jquery to any module that requires it
+});
