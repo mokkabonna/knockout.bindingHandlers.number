236ffe6147310de0308f4db1d98c36e5871d63b9
Martin Hansen <martin@martinhansen.no>
2015-02-14 16:16:18 +0100

    Using official knockout bower package

--- spec/test-main.js
+++ spec/test-main.js
@@ -1,7 +1,7 @@
 require.config({
   baseUrl: '/base',
   paths: {
-    knockout: 'bower_components/knockout.js/knockout',
+    knockout: 'bower_components/knockout/dist/knockout',
     jquery: 'bower_components/jquery/dist/jquery'
   }
 });
